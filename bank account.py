4ebb467b59ba12ac7e7dfa6a096f651db6e67587
Keerthi-priya653 <sweetiesweetie69177@gmail.com>
2026-04-16 11:32:31 +0530

    Add files via upload

---
+++ bank account.py
@@ -0,0 +1,15 @@
+class bankaccount:
+    def __init__(self,name, balance):
+        self.name=name
+        self.balance=balance
+    def deposit(self,amount):
+        self.balance=self.balance+amount
+    def withdraw(self,amount):
+        self.balance=self.balance-amount
+    def display(self): 
+        print(self.name,"balance is:",self.balance)
+ba=bankaccount( "CSE",10000)
+ba.deposit(1000)
+ba.withdraw(500)
+ba.display()
+        
